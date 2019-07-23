35cde7d2b3a5ab24e0f43ddc58cdf017e30d5dfb
Celal Cankaya <celalcankaya@outlook.com>
2019-07-23 17:22:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,15 @@ class Dog
   attr_accessor :mood
   attr_reader :name
 
+  @@all = []
+
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
+  end
+  def all
+    @@all
   end
 end
