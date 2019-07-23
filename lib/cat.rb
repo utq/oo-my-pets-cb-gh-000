094165e4ac9678e2cdd04d15c3bf34d11b5fc753
Celal Cankaya <celalcankaya@outlook.com>
2019-07-23 17:18:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,8 +2,9 @@ class Cat
   attr_accessor :mood
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
   end
 end
